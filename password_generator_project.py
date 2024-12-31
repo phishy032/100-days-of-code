729ae5c2d303b553b5c55afc6855fd1aacf484d8
phishy032 <joecheng032@gmail.com>
2024-12-31 04:53:51 -0500

    Update password_generator_project.py

--- password_generator_project.py
+++ password_generator_project.py
@@ -8,23 +8,6 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Easy Level
-# password = ""
-#
-# # nr_letters = 4
-# for char in range(0, nr_letters):
-#     password += random.choice(letters)
-#
-# for char in range(0, nr_symbols):
-#     password += random.choice(symbols)
-#
-# for char in range(0, nr_numbers):
-#     password += random.choice(numbers)
-#
-#
-# print(password)
-
-# Hard Level
 password_list = []
 
 for char in range(0, nr_letters):
@@ -36,9 +19,7 @@ for char in range(0, nr_symbols):
 for char in range(0, nr_numbers):
     password_list.append(random.choice(numbers))
 
-# print(password_list)
 random.shuffle(password_list)
-# print(password_list)
 
 password = ""
 for char in password_list:
